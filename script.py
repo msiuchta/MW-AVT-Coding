84c7b624000bdefdf476f5607caeb82ba6d24bcf
Eric Z <ericzh509@gmail.com>
2024-03-29 13:15:44 -0500

    Modified steering commands

--- script.py
+++ script.py
@@ -10,22 +10,29 @@ screen = pygame.display.set_mode((240, 240))
 pygame.display.set_caption('Fred')
 # (short for fredrick reed)
 
-pwm.start(0)
 #right
-en_a = 4
-in1 = 17
-in2 = 27
+rEn_a = 4
+rIn1 = 17
+rIn2 = 27
+
+#left
+lEn_a = 16
+lIn1 = 20
+lIn2 = 21
 
 IO.setwarnings(False)
 IO.setmode(IO.BCM)
 
-IO.setup(en_a,IO.OUT)
-IO.setup(in1,IO.OUT)
-IO.setup(in2,IO.OUT)
+# setup GPIO pins as output
+IO.setup(rEn_a,IO.OUT)
+IO.setup(rIn1,IO.OUT)
+IO.setup(rIn2,IO.OUT)
+IO.setup(lEn_a,IO.OUT)
+IO.setup(lIn1,IO.OUT)
+IO.setup(lIn2,IO.OUT)
 
 print("w/s: acceleration")
 print("a/d: steering")
-print("UP/DOWN: tilt")
 print("esc: exit")
 
 #hostname="google.com"
@@ -35,15 +42,15 @@ p=IO.PWM(4,100)
 w=pygame.key.get_pressed()[pygame.K_w]
 lPower = 0
 rPower = 0
-p.start(lPower)
-#q.start(rPower)
+p.start(rPower)
+#q.start(lPower)
 stop = False
 count=0
 
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-GPIO.output(in4,GPIO.LOW)
-GPIO.output(in3,GPIO.LOW)
+IO.output(rIn1,IO.LOW)
+IO.output(rIn2,IO.LOW)
+IO.output(lIn1,IO.LOW)
+IO.output(lIn2,IO.LOW)
 
 while (True):
     count+=1
@@ -58,107 +65,74 @@ while (True):
 
 	#	count=0
     time.sleep(0.02)
+	
     if stop == 1:
-		GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-
-				#GPIO.output(in4,GPIO.LOW)
-        		#GPIO.output(in3,GPIO.LOW)	
+		IO.output(rIn1,IO.LOW)
+        IO.output(rIn2,IO.LOW)
+		IO.output(lIn1,IO.LOW)
+        IO.output(lIn2,IO.LOW)
+			
         break
+
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == K_w:
 				lPower = 60
 				rPower = 60
-                GPIO.output(in1,GPIO.HIGH)
-                GPIO.output(in2,GPIO.LOW)
 
-				#GPIO.output(in4,GPIO.HIGH)
-        		#GPIO.output(in3,GPIO.LOW)
+                IO.output(rIn1,IO.HIGH)
+                IO.output(rIn2,IO.LOW)
+				IO.output(lIn1,IO.HIGH)
+        		IO.output(lIn2,IO.LOW)
 
 				print("onward")
+
             elif event.key == K_s:
 				lPower = 30
 				rPower = 30
-				GPIO.output(in1,GPIO.LOW)
-        		GPIO.output(in2,GPIO.HIGH)
 
-         		#GPIO.output(in4,GPIO.LOW)
-         		#GPIO.output(in3,GPIO.HIGH)
+				IO.output(rIn1,IO.LOW)
+        		IO.output(rIn2,IO.HIGH)
+         		IO.output(lIn1,IO.LOW)
+         		IO.output(lIn2,IO.HIGH)
 				
 				print("backward")
+
 			elif event.key == K_a:
 				lPower = 30
 				rPower = 60
-				GPIO.output(in1,GPIO.HIGH)
-        		GPIO.output(in2,GPIO.LOW)
 
-         		#GPIO.output(in4,GPIO.LOW)
-         		#GPIO.output(in3,GPIO.LOW)
+				IO.output(rIn1,IO.HIGH)
+        		IO.output(rIn2,IO.LOW)
+         		IO.output(lIn1,IO.HIGH)
+         		IO.output(lIn2,IO.LOW)
+
 				print("left")
+
 			elif event.key == K_d:
 				lPower = 60
 				rPower = 30
-				GPIO.output(in1,GPIO.LOW)
-        		GPIO.output(in2,GPIO.HIGH)
 
-        		#GPIO.output(in4,GPIO.LOW)
-        		#GPIO.output(in3,GPIO.LOW)
+				IO.output(rIn1,IO.LOW)
+        		IO.output(rIn2,IO.HIGH)
+        		IO.output(lIn1,IO.LOW)
+        		IO.output(lIn2,IO.HIGH)
+
 				print("right")
-            elif event.key == K_ESCAPE:
-				lPower=0
-				rPower=0
-                stop = True
-				print("dead")
-            p.ChangeDutyCycle(lPower)
-			#q.ChangeDutyCycle(rPower)
 
+            elif event.key == K_ESCAPE:
+				lPower = 0
+				rPower = 0
+                stop = True
+
+				print("dead")
+
+            p.ChangeDutyCycle(rPower)
+			#q.ChangeDutyCycle(lPower)
 
     	elif event.type == pygame.KEYUP:
 			p.ChangeDutyCycle(0)
 			#q.ChangeDutyCycle(0)
 
-		#	if((pygame.key.get_pressed()[pygame.K_w] != 0 and pygame.key.get_pressed()[pygame.K_a] != 0) or (pygame.key.get_pressed()[pygame.K_w] !=0 and pygame.key.get_pressed()[pygame.K_d] != 0)):
-		#		GPIO.output(in1,GPIO.LOW)
-         #       GPIO.output(in2,GPIO.LOW)
-#
-#				#GPIO.output(in4,GPIO.LOW)
- #       		#GPIO.output(in3,GPIO.LOW)		
-#			elif((pygame.key.get_pressed()[pygame.K_s] != 0 and pygame.key.get_pressed()[pygame.K_a] != 0) or (pygame.key.get_pressed()[pygame.K_s] !=0 and pygame.key.get_pressed()[pygame.K_d] != 0)):
-#				GPIO.output(in1,GPIO.LOW)
- #               GPIO.output(in2,GPIO.LOW)
-#
-#				#GPIO.output(in4,GPIO.LOW)
- #       		#GPIO.output(in3,GPIO.LOW)	
-###              GPIO.output(in2,GPIO.LOW)
-#
-#				#GPIO.output(in4,GPIO.LOW)
- #       		#GPIO.output(in3,GPIO.LOW)	
-#			elif(pygame.key.get_pressed()[pygame.K_s] != 0):
-##               GPIO.output(in2,GPIO.LOW)
-#
-#				#GPIO.output(in4,GPIO.LOW)
- #       		#GPIO.output(in3,GPIO.LOW)	
-#			elif(pygame.key.get_pressed()[pygame.K_a] != 0):
-#				GPIO.output(in1,GPIO.LOW)
- #               GPIO.output(in2,GPIO.LOW)
-#
-#				#GPIO.output(in4,GPIO.LOW)
-        		#GPIO.output(in3,GPIO.LOW)	
-#			elif(pygame.key.get_pressed()[pygame.K_d] != 0):
-#				GPIO.output(in1,GPIO.LOW)
- #               GPIO.output(in2,GPIO.LOW)
-
-				#GPIO.output(in4,GPIO.LOW)
-        		#GPIO.output(in3,GPIO.LOW)	
-#			else:
-#				GPIO.output(in1,GPIO.LOW)
- #               GPIO.output(in2,GPIO.LOW)
-
-				#GPIO.output(in4,GPIO.LOW)
-        		#GPIO.output(in3,GPIO.LOW)	
-#			t.ChangeDutyCycle(throttle)
-#			s.ChangeDutyCycle(steer)
-		
             
         
